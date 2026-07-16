b93e26ecf8f1e838499b6417c20188f6e45e314a
Jibtea <jkunyaphila@gmail.com>
2026-07-16 16:43:10 +0700

    change AI model

--- chatbot/services/geminiService.mjs
+++ chatbot/services/geminiService.mjs
@@ -11,7 +11,7 @@ export async function askGemini(prompt) {
 
     const response =
         await ai.models.generateContent({
-            model: "gemini-2.5-flash",
+            model: "gemini-3.1-flash-lite",
             contents: prompt
         });
 
