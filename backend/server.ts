c62117ae326caa2e2a72cdf85520f658a3bc95d1
naturetechlearndo <naturetech.learndo@gmail.com>
2026-09-24 13:47:27 +0700

    Update server.ts add "https://www.travellocal.online" line 22 to 25

--- backend/server.ts
+++ backend/server.ts
@@ -20,8 +20,8 @@ import { optionalVerifyToken, verifyToken } from "./middlewares/authMiddleware";
 const app = express();
 
 const allowedOrigins = process.env.FRONTEND_URL
-  ? [process.env.FRONTEND_URL, "http://localhost:5173", "http://localhost:5174"]
-  : ["http://localhost:5173", "http://localhost:5174"];
+  ? [process.env.FRONTEND_URL,"https://www.travellocal.online", "http://localhost:5173", "http://localhost:5174"]
+  : ["https://www.travellocal.online", "http://localhost:5173", "http://localhost:5174"];
 app.use(cors({ origin: allowedOrigins }));
 app.use(express.json());
 
